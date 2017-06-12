5eeccef01419ce0be41c3d47c7bab3bf1513fd57
chenrongxin <848536190@qq.com>
2017-06-12 17:07:11 +0800

    0612_work

--- wp-content/themes/reviewzine/functions.php
+++ wp-content/themes/reviewzine/functions.php
@@ -476,7 +476,7 @@ add_filter( 'get_the_archive_title', function ( $title ) {
 
 	if ( is_category() ) {
 		$title = single_cat_title( '', false );
-		return __( 'Category', 'reviewzine' ) . '<span class="category-name">' . esc_html( $title ) . '</span>';
+		return __( '', '' ) . '<span class="category-name">' . esc_html( $title ) . '</span>';
 	}
 
 	return $title;
