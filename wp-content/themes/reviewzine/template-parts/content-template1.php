626492570daef99d517e6d011179e5eeca40aefb
chenrongxin <848536190@qq.com>
2017-06-21 09:34:59 +0800

    0621_回退，备份

--- wp-content/themes/reviewzine/template-parts/content-template1.php
+++ wp-content/themes/reviewzine/template-parts/content-template1.php
@@ -19,22 +19,45 @@ $wp_query = new WP_Query(
 if ( $wp_query->have_posts() ) : ?>
 	<div class="post-section islemag-template1">
 
-	<ul class="article-list">
+	<div class="owl-carousel islemag-template1-posts smaller-nav no-radius">
 		<?php
-		
+
 		while ( $wp_query->have_posts() ) : $wp_query->the_post();
 			$category = get_the_category();
 			$postid = get_the_ID();
 		?>
-		<li>
-			<span class="post-title">
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" target="_blank"><?php the_title(); ?></a>
-            </span>
-            <!-- 显示发布日期 -->
-            <span class="post-date"><i class="fa fa-calendar-o" style="padding-right: 2px;color: #8d8d8d;"></i><?php echo esc_html( get_the_date() ); ?></span>
-        </li>
-			
 
+		  <article class="entry entry-overlay entry-block">
+			<a href="<?php echo esc_url( get_category_link( $category[0]->cat_ID ) );?>" class="category-block" title="Category <?php echo esc_attr( $category[0]->cat_name ); ?>"><?php echo esc_attr( $category[0]->cat_name ); ?></a>
+			<div class="entry-media">
+			  <figure>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+					<?php
+					$thumb_id = get_post_thumbnail_id( $wp_query->ID );
+					$thumb_meta = wp_get_attachment_metadata( $thumb_id );
+					if ( ! empty( $thumb_id ) ) {
+						if ( $thumb_meta['width'] / $thumb_meta['height'] > 1 ) {
+							$thumb = wp_get_attachment_image_src( $thumb_id, 'islemag_section4_big_thumbnail' );
+							$url = $thumb['0'];
+						} else {
+							$thumb = wp_get_attachment_image_src( $thumb_id, 'islemag_section4_big_thumbnail_no_crop' );
+							$url = $thumb['0'];
+						}
+						echo '<img class="owl-lazy" data-src="' . esc_url( $url ) . '" />';
+					} else {
+						echo '<img class="owl-lazy" data-src="' . get_template_directory_uri() . '/img/placeholder-image.png" />';
+					}
+					?>
+				</a>
+			  </figure> <!-- End figure -->
+			</div> <!-- End .entry-media -->
+
+			<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<div class="entry-meta">
+				<span class="entry-overlay-date"><i class="fa fa-calendar-o"></i><?php echo get_the_date( 'j F Y' ); ?></span>
+				<span class="entry-separator">|</span>
+				<a href="<?php the_permalink(); ?>" class="entry-comments"><i class="fa fa-comment-o"></i><?php comments_number( '0', '1', '%' ); ?></a>
+			</div>
 			<?php
 			if ( function_exists( 'cwppos_calc_overall_rating' ) ) {
 				$rating = cwppos_calc_overall_rating( $postid );
@@ -58,12 +81,11 @@ if ( $wp_query->have_posts() ) : ?>
 				<?php
 				}
 			} ?>
-
+		  </article> <!-- End .entry-overlay -->
 		<?php
 		  endwhile;
 		?>
-		</ul>
-
+	</div> <!-- End .islemag-template1-posts -->
 	</div> <!-- End .islemag-template1 -->
 <?php
 	endif;
