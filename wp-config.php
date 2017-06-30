ae4fbd9a32eb2829b799dee26790d452bdfce0c7
chenrongxin <848536190@qq.com>
2017-06-30 08:34:38 +0800

    0630_first

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'youtrytry');
+define('DB_PASSWORD', '');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
